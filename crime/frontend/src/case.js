adf9eea29466ca8478ad38fcb4d463c4474da7c9
talryan <nataliamryan@gmail.com>
2021-02-18 12:17:52 -0800

    Cases.js updated

--- crime/frontend/src/case.js
+++ crime/frontend/src/case.js
@@ -1,10 +1,12 @@
 class Case {
+    static cases = [];
 constructor(name, victim, bio, solved, category){
 this.name = name
 this.victim = victim
 this.bio = bio 
 this.solved = solved
 this.category = category 
+cases.push(this);
 }
 
 }

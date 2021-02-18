28cede270ee6a60c90458d06657d1ee460646440
talryan <nataliamryan@gmail.com>
2021-02-18 14:47:33 -0800

    createForm created.

--- crime/frontend/src/case.js
+++ crime/frontend/src/case.js
@@ -1,13 +1,30 @@
 class Case {
     static cases = [];
-constructor(knownAs, victim, bio, solved, category){
-this.knownAs= knownAs
-this.victim = victim
-this.bio = bio 
-this.solved = solved
-this.category = category 
-cases.push(this);
-}
+    constructor(knownAs, victim, bio, solved, category){
+        this.knownAs= knownAs
+        this.victim = victim
+        this.bio = bio 
+        this.solved = solved
+        this.category = category 
+        Case.cases.push(this);
+    }
 
-}
+
+
+
+    renderCase() {
+        let caseDiv = document.getElementById("cases-container")
+        caseDiv.innerHTML += 
+    `
+    <ul>
+    <li> Killer Name: ${this.knownAs} 
+    </li>
+    </ul>
+    `
+
+    }
+}       
+
+
+
 
--- crime/frontend/src/index.js
+++ crime/frontend/src/index.js
@@ -1,17 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchCases()
 })
+
 const baseUrl = "http://localhost:3000"
 
 function fetchCases(){
     fetch(`${baseUrl}/cases`)
     .then(response => response.json())
     .then(cases => { for (const trueCrimeCase of cases){
-        console.log("ro", trueCrimeCase)
+
         let c = new Case (trueCrimeCase.knownAs, trueCrimeCase.victim, trueCrimeCase.bio, trueCrimeCase.solved, trueCrimeCase.category)
-        console.log("js", c)
+       c.renderCase();
     }
 })
 }
 
+
+function createForm(){
+    let caseForm = document.getElementById("case-form")
+
+    caseForm.innerHTML += 
+    `
+    <form>
+        Name: <input type="text" id="knownAs"><br>
+        Victim: 
+        <input type="radio" id="victim"><br>
+        <label for="victim">Yes</label> <br>
+        <label for="victim">No</label> <br>
+        Bio: <input type="text" id="bio"><br>
+      
+        Solved?: <input type="radio" id="solved"><br>
+        <label for="solved">Yes</label> <br>
+        <label for="solved">No</label> <br>
+        Category: <input type="radio" id="solved"><br>
+        <label for="category"> Missing Person</label> <br>
+        <label for="category">Criminal</label> <br>
+        <label for="category">Serial Killer</label> <br>
+        <label for="category">Unsolved Mystery</label> <br>
+        <label for="category">Closed Case</label> <br>
+        <input type="submit" value="Create Case">
+    </form>
+    `
+
+    caseForm.addEventListener("submit", caseFormSubmission)
+
+}
 

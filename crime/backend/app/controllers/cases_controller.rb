93df41da50277b594bc79146c4f9ea75acc027aa
talryan <nataliamryan@gmail.com>
2021-02-18 12:15:43 -0800

    JS Case Class Created.

--- crime/backend/app/controllers/cases_controller.rb
+++ crime/backend/app/controllers/cases_controller.rb
@@ -1,7 +1,5 @@
 class CasesController < ApplicationController
 
-
-
   def index
     cases = Case.all
 
---
+++ crime/frontend/src/case.js
@@ -0,0 +1,11 @@
+class Case {
+constructor(name, victim, bio, solved, category){
+this.name = name
+this.victim = victim
+this.bio = bio 
+this.solved = solved
+this.category = category 
+}
+
+}
+
